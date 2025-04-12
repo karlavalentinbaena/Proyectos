2b9c1bf5bd4390770cec12785c298dd8789386d2
Karla Valentin Baena <92067163+karlavalentinbaena@users.noreply.github.com>
2025-04-12 06:47:34 -0600

    app.js

--- NoSQL/Frontend/app.js
+++ NoSQL/Frontend/app.js
@@ -1,20 +1,24 @@
 document.getElementById('data').addEventListener('submit', (e) => {
-    e.preventDefault()
-   
-    data_body = { "username": document.querySelector("#userName").value, "password": document.querySelector("#userpwd").value}
-   fetch('http://localhost:3000/login', {
-    method: 'POST',
-    body: JSON.stringify(data_body),
-    headers: {
-    "Content-Type": "application/json",
-    },
-    }).then(response => response.json())
-    .then(data => {
-    if(data.mdb_data === null) {
-    alert("Los datos están incorrectos")
-    }
-    else{
-    alert("Los datos están correctos")
-    }
-    })x
+    e.preventDefault();
+
+    const data_body = {
+        user: document.querySelector("#userName").value,
+        password: document.querySelector("#userpwd").value
+    };
+
+    fetch('http://localhost:3000/login', {
+        method: 'POST',
+        body: JSON.stringify(data_body),
+        headers: {
+            "Content-Type": "application/json"
+        }
     })
+    .then(response => response.json())
+    .then(data => {
+        if (data.mdb_data === null) {
+            alert("Datos incorrectos");
+        } else {
+            alert("Datos correctos");
+        }
+    });
+});
