ad4c5dbc19d4ce84545fca5226b05b3133b99866
Karla Valentin Baena <92067163+karlavalentinbaena@users.noreply.github.com>
2025-04-12 06:48:58 -0600

    server.js

--- NoSQL/Backend/server.js
+++ NoSQL/Backend/server.js
@@ -1,33 +1,33 @@
-import { MongoDbConnection } from './bd.js';
-import express, { json } from 'express'
-import cors from 'cors'
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+const ConectarMongoDb = require('./bd.js');
 
-app.use(express.urlencoded({extended:true}))
+const app = express();
+const port = 3000;
 
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(
-cors (
-{
-'origin': ['http://127.0.0.1:5500', 'http://localhost:5500', 'http://127.0.0.1:3000', 'http://localhost:3000'],
-preflightContinue:true
-}
-)
-)
+app.use(cors({
+  origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+  preflightContinue: true
+}));
 
 app.listen(port, () => {
-console.log(`Example app listening on port ${port}`);
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
-app.post('/login',async (req, res)=>{
-console.log(req.body)
+app.post('/login', async (req, res) => {
+  console.log(req.body);
 
-const conn_db = await new MongoDbConnection('test', 'users')
-let data = await conn_db.find_one({email:req.body.user, password:req.body.password})
+  const conn_db = new ConectarMongoDb('test', 'users');
+  await conn_db.connect();
 
-console.log(data)
-res.setHeader('Content-Type', 'application/json');
-    return res.json({message: 'información encontrada', mdb_data:data})
-    })
+  let data = await conn_db.findOne({ email: req.body.user, password: req.body.password });
+
+  console.log(data);
+
+  res.setHeader('Content-Type', 'application/json');
+  return res.json({ message: 'la informacion es', mdb_data: data });
+});
+
