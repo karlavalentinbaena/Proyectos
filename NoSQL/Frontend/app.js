c6ccabac470425955fc68883a9248b1fbd1365de
Karla Valentin Baena <92067163+karlavalentinbaena@users.noreply.github.com>
2025-04-12 06:35:03 -0600

    app.js

--- NoSQL/Frontend/app.js
+++ NoSQL/Frontend/app.js
@@ -1,13 +1,20 @@
-async function login() {
-    const usuario = document.getElementById("usuario").value;
-    const contrasena = document.getElementById("contrasena").value;
-
-    const res = await fetch("http://localhost:3000/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ usuario, contrasena })
-    });
-
-    const text = await res.text();
-    document.getElementById("respuesta").textContent = text;
-}
+document.getElementById('data').addEventListener('submit', (e) => {
+    e.preventDefault()
+   
+    data_body = { "username": document.querySelector("#userName").value, "password": document.querySelector("#userpwd").value}
+   fetch('http://localhost:3000/login', {
+    method: 'POST',
+    body: JSON.stringify(data_body),
+    headers: {
+    "Content-Type": "application/json",
+    },
+    }).then(response => response.json())
+    .then(data => {
+    if(data.mdb_data === null) {
+    alert("Los datos están incorrectos")
+    }
+    else{
+    alert("Los datos están correctos")
+    }
+    })x
+    })
