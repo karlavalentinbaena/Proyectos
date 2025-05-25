f93d6536d39cbff7a725186c43e221e139e2cb84
Karla Valentin Baena <92067163+karlavalentinbaena@users.noreply.github.com>
2025-05-24 19:38:37 -0600

    bd.js

--- NoSQL/Backend/bd.js
+++ NoSQL/Backend/bd.js
@@ -4,21 +4,28 @@ const { MongoClient } = require("mongodb");
 class ConectarMongoDb {
     constructor(db, collection) {
         //el usuario y contraseña que se pidió con la url para conectar a mongo
-        this.uri = "mongodb+srv://student:dPgF0sb0ADBUZHCI@clusterunam.6pxlppf.mongodb.net/?retryWrites=true&w=majority&appName=ClusterUNAM";
+        this.uri = "mongodb+srv://student:dPgF0sb0ADBUZHCI@clusterunam.6pxlppf.mongodb.net/?retryWrites=true&w=majority&appName=ClusterUNAM"
         this.db = db;
         this.collection = collection;
         this.client = new MongoClient(this.uri);
     }
 
     async connect() {
-        await this.client.connect();
-        this.basedatos = this.client.db(this.db);
-        this.col = this.basedatos.collection(this.collection);
+        try {
+            await this.client.connect();
+            this.basedatos = this.client.db(this.db);
+            this.col = this.basedatos.collection(this.collection);
+            console.log("Conexión a mongo bien");
+    }catch (error) {
+        console.error("Error en la conexion de mongo:", error);
     }
-
+}    
     async findOne(query) {
         return await this.col.findOne(query);
     }
 }
 
 module.exports = ConectarMongoDb;
+
+
+
