c1c0949df5c2dd93932f038027f82b0ff4f2afa2
Karla Valentin Baena <92067163+karlavalentinbaena@users.noreply.github.com>
2025-05-24 21:07:11 -0600

    app.js

--- NoSQL/Frontend/app.js
+++ NoSQL/Frontend/app.js
@@ -7,7 +7,7 @@ async function login() {
         usuario = JSON.parse(usuario);
         contrasena = JSON.parse(contrasena);
     } catch (e) {
-        //y sino, se queda como un string 
+        //y sino, se queda como un string
     }
 
     const res = await fetch("http://localhost:3000/login", {
@@ -17,7 +17,40 @@ async function login() {
     });
 
     const text = await res.text();
-    document.getElementById("respuesta").textContent = text;
+    const mensaje = document.getElementById("respuesta");
+    mensaje.textContent = text;
+
+    if (text.includes("correcto")) {
+        mensaje.className = "mensaje deexito";
+    } else {
+        mensaje.className = "mensaje de error";
+    }
 }
 
-});
+//Para poder hacer el registro
+function registrarse() {
+    const usuario = document.getElementById("nuevoUsuario").value;
+    const contrasena = document.getElementById("nuevaContrasena").value;
+    const mensaje = document.getElementById("respuestaRegistro");
+
+    fetch("http://localhost:3000/registro", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ usuario, contrasena })
+    })
+    .then(response => response.text())
+    .then(data => {
+        mensaje.textContent = data;
+
+        if (data.includes("exitoso")) {
+            mensaje.className = "mensaje de exito";
+        } else {
+            mensaje.className = "mensaje de error";
+        }
+    })
+    .catch(error => {
+        mensaje.textContent = "Error en el registro";
+        mensaje.className = "mensaje error";
+        console.error("Error:", error);
+    });
+}
